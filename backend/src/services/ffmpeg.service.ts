4568ddf6a62c25e3d282c846c3246759d922e033
Thomas Kottke <t.kottke90@gmail.com>
2023-12-01 12:48:23 -0600

    refactor: Add convert fn to ffmpeg service

--- backend/src/services/ffmpeg.service.ts
+++ backend/src/services/ffmpeg.service.ts
@@ -31,6 +31,29 @@ export class FfmpegService {
     return result;
   }
 
+  async convert(filePath: string, targetFormat: string) {
+    if (filePath.length === 0) {
+      throw new BadRequestError('No File Provided');
+    }
+
+    const result = await new Promise<string>((res, rej) => {
+      const outputFile = this.replaceExtension(filePath);
+
+      exec(`ffmpeg -i ${filePath} ${outputFile}`, (error, stdout, stderr) => {
+        if (error) {
+          rej(error);
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+
+        res(outputFile);
+      });
+    });
+
+    console.log(result);
+    return result;
+  }
+
   private replaceExtension(filename: string) {
     const base = basename(filename);
     const path = dirname(filename);
